7cdbd903df3461d0448268a38ea1e71fba474404
shawns21 <shawnsinanan21@gmail.com>
2022-02-07 21:41:22 -0500

    first commit

---
+++ 2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int first_int;
+int second_int;
+
+cout << "enter first int: \n";
+cin >> first_int;
+cout << "enter second int: \n";
+cin >> second_int;
+
+if (second_int % first_int == 0){
+
+cout << first_int << " is a multiple of " << second_int << "\n"; 
+
+}
+
+else{
+
+cout << first_int << " is not a multiple of " << second_int << "\n";
+}
+
+for (int i = 0; i < 4; i++){
+
+cout << "* * * * * * * *\n";
+cout << " * * * * * * * *\n";
+
+}
+
+cout << "*********         ***               *               *             \n";
+cout << "*       *     *         *          ***             * *            \n";
+cout << "*       *    *           *        *****           *   *           \n";
+cout << "*       *    *           *          *            *     *          \n";
+cout << "*       *    *           *          *           *       *         \n";
+cout << "*       *    *           *          *            *     *          \n";
+cout << "*       *    *           *          *             *   *           \n";
+cout << "*       *     *         *           *              * *            \n";
+cout << "*********         ***               *               *             \n";
+
+}
